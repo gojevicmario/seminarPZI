cef4d031d55c817df7954faf9f137c29be4e2a2d
gojevicmario <gojevic.mario@gmail.com>
2018-01-21 17:04:37 +0100

    Changed the gallery.js

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -2,21 +2,24 @@ $(document).ready(function(){
     var cnt=2;
     $( ".next" ).click(function() {
         $(".gallery-active").fadeOut("fast",function(){
+            cnt++;
+            if(cnt>6)
+                cnt=1;
             $(this).attr("src","../img/Josip/"+cnt+".jpg");
             $(this).fadeIn("fast");
-            cnt++;
+
         });
-        if(cnt>6)
-            cnt=1;
+        
         });
     $(".prev").click(function(){
+        cnt--;
+        if(cnt < 1)
+            cnt=6;
         $(".gallery-active").fadeOut("fast",function(){
             $(this).attr("src","../img/Josip/"+cnt+".jpg");
             $(this).fadeIn("fast");
-            cnt--;
         });
-        if(cnt < 1)
-            cnt=6;
+        
     });
 
 });
